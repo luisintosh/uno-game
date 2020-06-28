bf974ce475619281acf75e541a5d01569123bf77
Luis Mendieta <luismepa@gmail.com>
2020-06-28 10:59:01 -0500

    feat: add layouts to manage the UI

---
+++ src/app/shared/components/modal-layout/modal-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-layout',
+  templateUrl: './modal-layout.component.html',
+  styleUrls: ['./modal-layout.component.scss'],
+})
+export class ModalLayoutComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
